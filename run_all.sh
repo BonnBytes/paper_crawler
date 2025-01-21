72b22f6256992764c94e9a300ca9cdeb274ec48c
Moritz Wolter <moritz@wolter.tech>
2025-01-21 19:19:14 +0100

    runall update.

--- run_all.sh
+++ run_all.sh
@@ -1,2 +1,4 @@
+python -m src.paper_crawler.crawl_links_soup
+python -m src.paper_crawler.crawl_links_openreview
 python -m src.paper_crawler.filter_and_download_links
 python -m src.paper_crawler.filter_and_download_links --id ICLR.cc/2024/Conference
