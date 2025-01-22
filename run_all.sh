b65153f3275981ef6b102b885b8c42a1270794d7
Moritz Wolter <moritz@wolter.tech>
2025-01-22 16:10:56 +0100

    add icml2023

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 python -m src.paper_crawler.crawl_links_soup
+python -m src.paper_crawler.crawl_links_soup --id icml2023
 python -m src.paper_crawler.crawl_links_openreview --id ICLR.cc/2024/Conference
-python -m src.paper_crawler.crawl_links_openreview --id ICLR.cc/2023/Conference
 python -m src.paper_crawler.filter_and_download_links
 python -m src.paper_crawler.filter_and_download_links --id ICLR.cc/2024/Conference
