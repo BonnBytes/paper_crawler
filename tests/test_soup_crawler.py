817f411191ab2e925a3d57c749d0180afef27499
Moritz Wolter <moritz@wolter.tech>
2025-02-03 12:30:15 +0100

    remove unused ignores.

--- tests/test_soup_crawler.py
+++ tests/test_soup_crawler.py
@@ -29,7 +29,7 @@ def test_process_link() -> None:
     """Check if the process link function works ok."""
     link = "https://proceedings.mlr.press/v139/achituve21a/achituve21a.pdf"
     assert (
-        urllib.parse.urlunparse(process_link(link)[0])  # type: ignore
+        urllib.parse.urlunparse(process_link(link)[0])
         == "https://github.com/IdanAchituve/GP-Tree"
     )
 
